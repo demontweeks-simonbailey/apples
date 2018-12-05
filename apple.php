ea8c10cd25677f1abe102df699f8c3f743077d51
Simon Bailey <simon.bailey@parcelpet.com>
2018-12-05 14:50:43 +0000

    Update via PHPStorm

--- apple.php
+++ apple.php
@@ -2,4 +2,5 @@
 $apples = "I like apples<br />";
 $apples .= "why not have an apple? (apple dependency)<br />";
 $apples .= "an apple a days keeps the doctor away<br />";
+$apples .= "more apples<br />";
 ?>
