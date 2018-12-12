170a411309799718bd2ec52ee3fa40e3bfad1f2a
Simon Bailey <simon.bailey@parcelpet.com>
2018-12-12 08:59:19 +0000

    Added new line

--- apple.php
+++ apple.php
@@ -3,5 +3,6 @@ $apples = "I like apples<br />";
 $apples .= "why not have an apple? (apple dependency)<br />";
 $apples .= "an apple a days keeps the doctor away<br />";
 $apples .= "more apples<br />";
-$apples .= "granny smith<br />"
+$apples .= "granny smith<br />";
+$apples .= "sliced in fruit salad";
 ?>
